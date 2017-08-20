ad13239d4b473d3084295e107aad7ee330d6dea9
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-08-19 22:44:50 -0400

    Added test.

--- CI-TestTests/CI_TestTests.swift
+++ CI-TestTests/CI_TestTests.swift
@@ -20,17 +20,11 @@ class CI_TestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+   
+    func testEquality() {
+        let x = 1
+        let y = 2 - 1
+        XCTAssert(x == y)
     }
     
 }
